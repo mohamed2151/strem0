2ace31b8cfbe73a56b61930f29ae40426895b356
mohamed2151 <44812878+mohamed2151@users.noreply.github.com>
2018-12-07 12:10:49 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Darks`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By :Darks  ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`DeexIsHere`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("");
